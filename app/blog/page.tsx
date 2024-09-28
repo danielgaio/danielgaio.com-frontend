498e0fbb97cbd82c98a684a3822fac95044091d1
Daniel <danielelielgaio@gmail.com>
2024-09-28 08:32:59 -0300

    feat: Changed tab icon and tab names for existing pages

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,4 +1,12 @@
+"use client";
+
+import { useEffect } from "react";
+
 export default function Blog() {
+  useEffect(() => {
+    document.title = "Daniel's Blog";
+  }, []);
+
   return (
     <main className="flex flex-col items-center justify-center min-h-screen p-6 sm:p-12">
       <h1>My blog</h1>
--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,4 +1,12 @@
+"use client";
+
+import { useEffect } from "react";
+
 export default function Contact() {
+  useEffect(() => {
+    document.title = "Contact Daniel";
+  }, []);
+
   return (
     <main className="flex flex-col items-center justify-center min-h-screen p-6 sm:p-12">
       <h1>Contact me</h1>
